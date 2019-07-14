406b4b10f6e34f68ef41cc72e9fc84dcfbe1e061
chunsheng <chunsheng.liu@eingsoft.com>
2019-07-14 13:04:53 +0800

    init

---
+++ springboot-static-resources/src/main/java/com/brotherc/springbootstaticresources/SpringbootStaticResourcesApplication.java
@@ -0,0 +1,13 @@
+package com.brotherc.springbootstaticresources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStaticResourcesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStaticResourcesApplication.class, args);
+    }
+
+}
