2bfd319c6d54942a92e196fda50d2082655d9250
chunsheng <chunsheng.liu@eingsoft.com>
2019-07-14 16:10:09 +0800

    init

--- springboot-jsp/src/main/java/com/brotherc/springbootjsp/controller/TeacherController.java
+++ springboot-jsp/src/main/java/com/brotherc/springbootjsp/controller/TeacherController.java
@@ -1,11 +1,13 @@
 package com.brotherc.springbootjsp.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author: Brotherc
  * @date: 2019/7/14 14:10
  */
+@Controller
 @RequestMapping("/teacher")
 public class TeacherController {
 
