3bd1432af83feaf20dd12e578777649a663d7326
chunsheng <chunsheng.liu@eingsoft.com>
2019-07-15 10:21:39 +0800

    init

--- springboot-global-exception/src/main/java/com/brotherc/springbootglobalexception/exception/GlobalExceptionHandler.java
+++ springboot-global-exception/src/main/java/com/brotherc/springbootglobalexception/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.brotherc.springbootglobalexception.exception;
 
 import java.util.HashMap;
 import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,7 +17,13 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)//捕获运行时异常
     @ResponseBody
-    public Map<String,Object> exceptionHander(){
+    public Map<String,Object> exceptionHander(HttpServletRequest req, Exception e){
+        // 如果返回的是View，方法的返回值低ModelAndView
+        // ModelAndView modelAndView = new ModelAndView();
+        // modelAndView.setView();
+        // return modelAndView;
+
+        // 如果返回值是String或json，那么需要在方法上添加ResponseBody注解
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("errorCode", "101");
         map.put("errorMsg", "系統错误!");
