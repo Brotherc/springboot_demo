512e18897964dd62cd79dac4fd9806b5c0f720c3
chunsheng <chunsheng.liu@eingsoft.com>
2019-07-15 10:26:07 +0800

    init

--- springboot-web/src/main/java/com/brotherc/springbootweb/config/SrpingMVCConfig.java
+++ springboot-web/src/main/java/com/brotherc/springbootweb/config/SrpingMVCConfig.java
@@ -1,17 +1,19 @@
 package com.brotherc.springbootweb.config;
 
-import java.nio.charset.Charset;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 
 /**
  * @author: Brotherc
@@ -48,7 +50,16 @@ public class SrpingMVCConfig extends WebMvcConfigurerAdapter {
     // 自定义消息转化器的第二种方法
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        StringHttpMessageConverter converter  = new StringHttpMessageConverter(Charset.forName("UTF-8"));
+        // 1.需要先定义一个convert转换消息的对象
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        // 2.添加fastJson的配置信息，比如：是否要格式化返回的json数据
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(
+                SerializerFeature.PrettyFormat
+        );
+        // 3.在convert中添加配置信息
+        converter.setFastJsonConfig(fastJsonConfig);
+
         converters.add(converter);
     }
 
