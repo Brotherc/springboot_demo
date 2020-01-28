8cd470349491904c83813e3550d1e1efe747765e
chunsheng <chunsheng.liu@eingsoft.com>
2020-01-28 13:08:09 +0800

    init

---
+++ springboot-web2/src/main/java/com/brotherc/springbootweb2/config/WebConfig.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package com.brotherc.springbootweb2.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.brotherc.springbootweb2.filter.TimeFilter;
+import com.brotherc.springbootweb2.interceptor.TimeInterceptor;
+
+/**
+ * @author zhailiang
+ *
+ */
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+	
+	@SuppressWarnings("unused")
+	@Autowired
+	private TimeInterceptor timeInterceptor;
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(timeInterceptor);
+	}
+	
+	@Bean
+	public FilterRegistrationBean timeFilter() {
+		
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+		
+		TimeFilter timeFilter = new TimeFilter();
+		registrationBean.setFilter(timeFilter);
+		
+		List<String> urls = new ArrayList<>();
+		urls.add("/*");
+		registrationBean.setUrlPatterns(urls);
+		
+		return registrationBean;
+		
+	}
+
+}
