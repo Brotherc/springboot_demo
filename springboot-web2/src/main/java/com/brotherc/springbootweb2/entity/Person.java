9aa8353339500043fc595ac131aa283b7a3a21a9
chunsheng <chunsheng.liu@eingsoft.com>
2020-01-28 13:08:49 +0800

    init

---
+++ springboot-web2/src/main/java/com/brotherc/springbootweb2/entity/Person.java
@@ -0,0 +1,24 @@
+package com.brotherc.springbootweb2.entity;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import com.alibaba.fastjson.annotation.JSONField;
+
+/**
+ * @author: Brotherc
+ * @date: 2019/7/14 20:33
+ */
+@Data
+@AllArgsConstructor
+public class Person {
+    private Integer id;
+    private String name;
+    @JSONField(format = "yyyy-MM-dd HH:mm")
+    private Date createTime;
+
+    @JSONField(serialize = false)
+    private String remark;
+}
